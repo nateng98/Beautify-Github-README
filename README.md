d64fb0b94644889e313210a05e2c31887f590aef
Nước Mắm Code <nathan.ng.0808@gmail.com>
2024-09-05 00:28:05 -0500

    reinitialized project with vite react instead of create react app

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Easy-Github-Profile
-Being creative and making your Github profile more appealing!
+# E-commerce Website
+
+### *Techstack: React (Typescript), Tailwind, NodeJS, Stripe.js, Firebase*
+<!-- pnpm i tailwind-merge: Utility function to efficiently merge Tailwind CSS classes in JS without style conflicts. -->
+
+
+
+
---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import Header from "./ui/Header"
+import Container from "./ui/Container"
+import Footer from "./ui/Footer"
+
+function App() {
+
+  return (
+    <main>
+      <Header></Header>
+      <Container>
+        <p>E-commerce Website</p>
+      </Container>
+      <Footer></Footer>
+    </main>
+  )
+}
+
+export default App
