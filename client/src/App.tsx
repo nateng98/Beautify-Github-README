c28b6d498e2417256ca29c2b0f6cc876ad2e3b42
Nước Mắm Code <nathan.ng.0808@gmail.com>
2024-09-05 22:19:50 -0500

    setup router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,12 @@
-import Header from "./ui/Header"
 import Container from "./ui/Container"
-import Footer from "./ui/Footer"
 
 function App() {
 
   return (
     <main>
-      <Header></Header>
       <Container>
         <p>E-commerce Website</p>
       </Container>
-      <Footer></Footer>
     </main>
   )
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,28 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Layout from './ui/Layout.tsx'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+
+const RouterLayout = () => {
+  return (
+    <Layout>
+      <Outlet />
+    </Layout>
+  )
+}
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <RouterLayout />,
+  children: [
+    {
+      path: '/',
+      element: <App />
+    },
+  ],
+}]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />
 )
