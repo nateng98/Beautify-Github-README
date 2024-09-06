6d886ee3630bf8f5ac41271d24b5ec9eadccd819
Nước Mắm Code <nathan.ng.0808@gmail.com>
2024-09-06 00:11:54 -0500

    initialized pages

--- client/src/ui/Footer.tsx
+++ client/src/ui/Footer.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Container from './Container'
+import { payment } from '../assets'
+import FooterTop from './FooterTop'
 
 const Footer = () => {
   return (
-	<div>Footer</div>
+	<div className='mt-10'>
+    <FooterTop />
+    <Container className='flex flex-col md:flex-row items-center gap-4 justify-between'>
+      <p>2024 &copy; nathaniel nguyen</p>
+      <img src={payment} alt="payment-img" />
+    </Container>
+  </div>
   )
 }
 
