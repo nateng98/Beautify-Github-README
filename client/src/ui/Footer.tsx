804e0bcda8f28a4f85f73f325639c6386079ab7f
Nước Mắm Code <nathan.ng.0808@gmail.com>
2024-09-06 19:11:11 -0500

    created footer, though I might need to style it later

--- client/src/ui/Footer.tsx
+++ client/src/ui/Footer.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Container from './Container'
 import { payment } from '../assets'
 import FooterTop from './FooterTop'
+import FooterNavigation from './FooterNavigation'
 
 const Footer = () => {
   return (
 	<div className='mt-10'>
     <FooterTop />
+    <FooterNavigation />
     <Container className='flex flex-col md:flex-row items-center gap-4 justify-between'>
       <p>2024 &copy; nathaniel nguyen</p>
       <img src={payment} alt="payment-img" />
