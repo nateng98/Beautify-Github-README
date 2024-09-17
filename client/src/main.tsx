395830f612a991786db363505fe823545c6f3649
Nước Mắm Code <77079010+nateng98@users.noreply.github.com>
2024-09-17 10:39:33 -0500

    Revert "Task/10 pages db api"

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,86 +1,10 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, Outlet, RouterProvider, ScrollRestoration } from 'react-router-dom'
 import App from './App.tsx'
-import Layout from './components/Layout.tsx'
 import './index.css'
-import Cancel from './pages/Cancel.tsx'
-import Cart from './pages/Cart.tsx'
-import Category from './pages/Category.tsx'
-import Favorite from './pages/Favorite.tsx'
-import NotFound from './pages/NotFound.tsx'
-import Orders from './pages/Orders.tsx'
-import Product from './pages/Product.tsx'
-import Profile from './pages/Profile.tsx'
-import Success from './pages/Success.tsx'
-
-const RouterLayout = () => {
-  return (
-    <Layout>
-      <ScrollRestoration />
-      {/*
-      Outlet is a placeholder component from react-router-dom that renders whatever child route matches the current path; allowing child routes to be nested within the Layout 
-      The child route component (App and components from pages) will be rendered here
-      */}
-      <Outlet /> 
-    </Layout>
-  )
-}
-
-const router = createBrowserRouter([{
-  path: '/', // the base URL of the app
-  element: <RouterLayout />, // every time the base URL is visited, <RouterLayout /> is rendered
-  children: [ // the child routes that will be rendered inside the Outlet of the RouterLayout
-    {
-      path: '/', // the home route
-      element: <App /> // the App component is rendered at the root path
-    },
-    {
-      path: '/product', // all products
-      element: <Product /> 
-    },
-    {
-      path: '/product/:id', // single product
-      element: <Product /> 
-    },
-    {
-      path: '/category',
-      element: <Category /> 
-    },
-    {
-      path: '/category/:id',
-      element: <Category /> 
-    },
-    {
-      path: '/profile',
-      element: <Profile /> 
-    },
-    {
-      path: '/cart',
-      element: <Cart /> 
-    },
-    {
-      path: '/orders',
-      element: <Orders /> 
-    },
-    {
-      path: '/favorite',
-      element: <Favorite /> 
-    },
-    {
-      path: '/success',
-      element: <Success /> 
-    },
-    {
-      path: '*',
-      element: <NotFound /> 
-    },
-    {
-      path: '/cancel',
-      element: <Cancel /> 
-    },
-  ],
-}]);
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
